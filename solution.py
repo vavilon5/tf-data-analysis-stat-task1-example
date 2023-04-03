544ba704469c4057c8de1509270505017801b668
vavilon5 <107186119+vavilon5@users.noreply.github.com>
2023-04-03 23:11:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 729362937 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return x.mean() + 15
