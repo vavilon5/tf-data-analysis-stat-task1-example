d1274b162155340d0eba4227e33449eba5e6859f
vavilon5 <107186119+vavilon5@users.noreply.github.com>
2023-04-07 21:35:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import numpy as np
 chat_id = 729362937 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    x = (x+25)/10
-    return x.mean() - 1
+    
+    return 2.4 + 0.1*x.mean()
